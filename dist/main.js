49f3acf930b680100992def03216db98cb6e1610
Vinit Hemadri <vineet.vh@gmail.com>
2021-11-22 00:03:31 +0530

    Added compute function

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("console.log(\"calculator\");\r\n\r\n\n\n//# sourceURL=webpack://calculator/./src/index.js?");
+eval("const operationsBtn = document.querySelectorAll(\"[data-operation]\");\r\nconst numbersBtn = document.querySelectorAll(\"[data-number]\");\r\nconst allClearBtn = document.querySelector(\"[data-all-clear]\");\r\nconst deleteBtn = document.querySelector(\"[data-delete]\");\r\nconst equalsBtn = document.querySelector(\"[data-equals\");\r\nconst previousOperandTextElement = document.querySelector(\"previous-operand\");\r\nconst currentOperandTextElement = document.querySelector(\".current-operand\");\r\n\r\nlet previous = 0;\r\nlet current = 0;\r\n\r\nallClearBtn.addEventListener(\"click\", () => {\r\n  currentOperandTextElement.textContent = \"\";\r\n  previousOperandTextElement.textContent = \"\";\r\n});\r\n\r\nconst compute = () => {\r\n  let computation;\r\n  switch (operation) {\r\n    case \"+\":\r\n      computation = previous + current;\r\n  }\r\n};\r\n\r\nnumbersBtn.forEach((button) => {\r\n  button.addEventListener(\"click\", () => {\r\n    previous += button.textContent;\r\n    currentOperandTextElement.textContent += button.textContent;\r\n  });\r\n});\r\n\r\noperationsBtn.forEach((button) => {\r\n  button.addEventListener(\"click\", () => {\r\n    currentOperandTextElement.textContent += button.textContent;\r\n    current += button.textContent;\r\n  });\r\n});\r\n\r\nequalsBtn.addEventListener(\"click\", () => {\r\n  compute();\r\n})\r\n\n\n//# sourceURL=webpack://calculator/./src/index.js?");
 
 /***/ })
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,41 @@
-console.log("calculator");
+const operationsBtn = document.querySelectorAll("[data-operation]");
+const numbersBtn = document.querySelectorAll("[data-number]");
+const allClearBtn = document.querySelector("[data-all-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const equalsBtn = document.querySelector("[data-equals");
+const previousOperandTextElement = document.querySelector("previous-operand");
+const currentOperandTextElement = document.querySelector(".current-operand");
 
+let previous = 0;
+let current = 0;
+
+allClearBtn.addEventListener("click", () => {
+  currentOperandTextElement.textContent = "";
+  previousOperandTextElement.textContent = "";
+});
+
+const compute = () => {
+  let computation;
+  switch (operation) {
+    case "+":
+      computation = previous + current;
+  }
+};
+
+numbersBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    previous += button.textContent;
+    currentOperandTextElement.textContent += button.textContent;
+  });
+});
+
+operationsBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    currentOperandTextElement.textContent += button.textContent;
+    current += button.textContent;
+  });
+});
+
+equalsBtn.addEventListener("click", () => {
+  compute();
+})
